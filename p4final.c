f3cc5d22696b21a177967fb941829888bbe90010
Vineeth B <94664950+vineeth2003@users.noreply.github.com>
2022-02-05 13:21:35 +0000

    p4final.c revision

---
+++ p4final.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int input_array_size()
+{
+  int n1;
+  printf("enter the size of the array\n");
+  scanf("%d", &n1);
+  return n1;
+}
+void input_array(int n, int a[n])
+{
+  int i, sum =0;
+  for(int i=0; i<n; i++){
+    printf("enter the element no %d of the array", i);
+    scanf("%d", &a[n]);
+  }
+}
+int sum_n_arrays(int n, int a[n])
+{
+  int i, sum = 0;
+  for(int i=0; i<n; i++)
+  {
+
+    sum = sum + a[i];
+    
+  }
+  return sum;
+}
+void out_put(int n, int a[n], int sum)
+{
+  int i;
+  for(int i=0; i<n; i++)
+  {
+    printf("%d", i);
+  }
+  printf("%d is %d", a[n-1], sum);
+}
+int main()
+{
+  int i, n, sum, a[i];
+  n = input_array_size(n,a);
+  input_array_size(n,a);
+  sum_n_arrays(n,a);
+  out_put(n,a,sum);
+}
